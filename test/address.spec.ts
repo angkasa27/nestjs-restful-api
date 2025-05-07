cadb1d4f85612ea5a9916b6a49dae5cabffb2e43
Dimas Angkasa Nurindra <mas.angkasa27@gmail.com>
2025-05-07 14:21:29 +0700

    Add list address API

--- test/address.spec.ts
+++ test/address.spec.ts
@@ -282,4 +282,42 @@ describe('AddressController', () => {
       expect(address).toBeNull();
     });
   });
+
+  describe('GET /api/contacts/:contactId/addresses', () => {
+    beforeEach(async () => {
+      await testService.deleteAll();
+      await testService.createUser();
+      await testService.createContact();
+      await testService.createAddress();
+    });
+
+    it('should be rejected if contact is not found', async () => {
+      const contact = await testService.getContact();
+      const response = await request(app.getHttpServer())
+        .get(`/api/contacts/${contact.id + 1}/addresses`)
+        .set('Authorization', 'test-token');
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(404);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    it('should be able to list address', async () => {
+      const contact = await testService.getContact();
+      const response = await request(app.getHttpServer())
+        .get(`/api/contacts/${contact.id}/addresses`)
+        .set('Authorization', 'test-token');
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(200);
+      expect(response.body.data.length).toBe(1);
+      expect(response.body.data[0].street).toBe('test');
+      expect(response.body.data[0].city).toBe('test');
+      expect(response.body.data[0].province).toBe('test');
+      expect(response.body.data[0].country).toBe('test');
+      expect(response.body.data[0].postal_code).toBe('1111');
+    });
+  });
 });
